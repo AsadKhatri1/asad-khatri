573e1401a9cc306184ad935f07413ad504baf942
AsadKhatri1 <92842688+AsadKhatri1@users.noreply.github.com>
2024-01-01 16:39:05 +0500

    1-1-24

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from '../comps/Hero';
 
 const About = () => {
   return (
     <div>
-      about
+      <Hero title="ASAD KHATRI" desc="I am a pofessional MERNSTACK DEVELOPER having experience of 3 years in the field. I design and develop web applications using MERN technology. I am running KATRIS CODE succssfully and catering numerous clients since 2020. Give us the chance to bring your business online." btnText="Order Now" link="/contact"/>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import Hero from '../comps/Hero';
 const Home = () => {
     return (
        <>
-       <Hero/>
+       <Hero title="KATRIS CODE" desc="I am a pofessional MERNSTACK DEVELOPER having experience of numerous years in the field. I design and develop web applications using MERN technology." btnText="Learn More"/>
        </>
     );
 }
