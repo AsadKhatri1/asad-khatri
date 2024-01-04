4f01f026af1a5a3d280db8d04f05836af6250c13
AsadKhatri1 <hafizasad398@gmail.com>
2024-01-04 17:41:16 +0500

    consult

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         title="KATRIS CODE"
         desc="Since 2020, we've been obsessed with crafting digital experiences that captivate audiences, fuel businesses, and dominate search engines. We're not just web developers and designers, we're architects of your online story, weaving together stunning visuals, cutting-edge tech, and a deep understanding of your brand to create websites that work as hard as you do. We obsess over user journeys, future-proof scalability, and dedicated support, because your success is our mission. Let's unleash your online potential. Contact us today for a free consultation and get ready to write a digital masterpiece."
         btnText="Consult Us"
-        link="/contact"
+        link="/consult"
       />
       <div>
         <WhyUs title="Why Choose Us" />
